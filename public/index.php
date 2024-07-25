436c91e4d712d73c350e9722bd691e2651022693
FernandoYZ <fernandozavala266@gmail.com>
2024-07-24 19:36:16 -0500

    build(view): add views

--- public/index.php
+++ public/index.php
@@ -1,9 +1,10 @@
-<?php 
+<?php
 
-require_once __DIR__ . '/../bootstrap/app.php';
+require __DIR__ . '/../bootstrap/app.php';
 
-$config = require_once __DIR__ . '/../config/app.php';
-
-$app = new App($config);
-
-$app -> handle($_SERVER['REQUEST_URI']);
+try {
+    $app->handle($_SERVER['REQUEST_URI']);
+} catch (\Exception $e) {
+    $handler = new \Core\ExceptionHandler();
+    $handler->handle($e);
+}
