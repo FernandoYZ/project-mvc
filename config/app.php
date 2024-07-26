c3f6dc04f23f57efb3b210ff49dca7603d1fa276
FernandoYZ <fernandozavala266@gmail.com>
2024-07-26 07:05:44 -0500

    [config]: add language in config Se agregó la variable APP_LANGUAGE de la variable de entorno.

--- config/app.php
+++ config/app.php
@@ -6,5 +6,5 @@ return [
     'debug' => filter_var(env('APP_DEBUG', false), FILTER_VALIDATE_BOOLEAN),
     'url' => env('APP_URL', 'http://localhost'),
     'currency' => env('APP_CURRENCY', 'USD'),
-    'language' => env('APP_LANGUAGE', 'en'),
+    'language' => env('APP_LANGUAGE', 'es'),
 ];
