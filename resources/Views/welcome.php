e60c1052a77188b86c61f27e3036d5f275335a91
FernandoYZ <fernandozavala266@gmail.com>
2024-07-26 11:56:52 -0500

    [welcome]: remove comentaries

--- resources/Views/welcome.php
+++ resources/Views/welcome.php
@@ -7,23 +7,17 @@
 
         <title>Proyecto - Bienvenido</title>
         
-        <!--================================= Fonts: Roboto ====================================-->
         <link rel="preconnect" href="https://fonts.googleapis.com">
-        <!--====================================================================================-->
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-        <!--======================== Integración de Tailwind con CND ===========================-->
+
         <script src="https://cdn.tailwindcss.com"></script>
-        <!--====================================================================================-->
         <script src="<?= media_js('tailwind.js') ?>"></script>
-        <!--====================================================================================-->
         <link href="<?= media_css('tailwind.css') ?>" type="text/tailwindcss" rel="stylesheet">
-        <!--====================================================================================-->
     </head>
 <body>
     <?php include layout('partials.header'); ?>
     <div class="container mt-5">
-        <?= $content ?? '' ?>
     </div>
     <?php include layout('partials.footer'); ?>
     <script src="<?= media_js('main.js') ?>" type="module"></script>
