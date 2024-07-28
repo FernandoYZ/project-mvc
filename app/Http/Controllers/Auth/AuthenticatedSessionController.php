36343205df99388d14fa0adc26fb5be38afeb636
FernandoYZ <fernandozavala266@gmail.com>
2024-07-28 08:34:20 -0500

    [auth]: added auth module to support routing and middleware structures

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Core\Controller;
+
+class AuthenticatedSessionController extends Controller
+{
+
+    public function create()
+    {}
+
+    public function store()
+    {}
+}
