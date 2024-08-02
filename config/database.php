c021db9015c144e6309f894a1cd088e94adcf9f2
FernandoYZ <fernandozavala266@gmail.com>
2024-08-02 12:46:02 -0500

    [database]: Settings have been updated Se actulizó el database de config para que reciba las variables del archivo .env y no directamente del array.

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ return [
     'driver' => env('DB_DRIVER', 'mysql'),
     'host' => env('DB_HOST', '127.0.0.1'),
     'port' => env('DB_PORT', '3306'),
-    'database' => env('DB_DATABASE', 'db_djr'),
+    'database' => env('DB_DATABASE', 'base_de_datos'),
     'username' => env('DB_USERNAME', 'root'),
     'password' => env('DB_PASSWORD', ''),
     'charset' => env('DB_CHARSET', 'utf8mb4'),
